8c924ff44fb3412f62316d9c48c5b0059a52ec18
Lee <lihaiboloveyou@live.com>
2016-12-19 09:35:53 +0800

    房间中人数为零时 删除房间(未修复)

--- service.js
+++ service.js
@@ -12,50 +12,64 @@ var onlineUsers = 0;
 
 var waitRoom = [];
 var room = {};
+var roomSize = 2;
 
 io.on('connection', function (socket) {
     onlineUsers++;
     console.log('有用户加入在线用户:' + onlineUsers);
     waitRoom.push(socket);
-    if (waitRoom.length == 5) {
+    if (waitRoom.length == roomSize) {
         var f = parseInt(10000 * Math.random()).toString();
         var wr = [];
         for (var i = 0; i < waitRoom.length; i++) {
             wr.push(waitRoom[i]);
         }
-        waitRoom.splice(0,waitRoom.length);
-        for(var i = 0;i<wr.length;i++){
-            wr[i].emit('j',{flag:f,num:i});
+        waitRoom.splice(0, waitRoom.length);
+        for (var i = 0; i < wr.length; i++) {
+            wr[i].emit('j', {flag: f, num: i});
         }
         room[f] = wr;
 
     }
 
     socket.on('message', function (obj) {
-        for(var i in room[obj.flag]){
-            room[obj.flag][i].emit('message',obj);
+        for (var i in room[obj.flag]) {
+            room[obj.flag][i].emit('message', obj);
         }
     });
 
     socket.on('leave', function (obj) {
         onlineUsers--;
-        console.log('有玩家离开，当前在线玩家:'+ onlineUsers);
-        for(var i in room[obj.flag]){
-            room[obj.flag][i].emit('leave',obj);
+        if (obj.flag != null) {
+            for (var i in room[obj.flag]) {
+                room[obj.flag][i].emit('leave', obj);
+            }
+            removeByValue(room[obj.flag], obj.num);
+            if(room[obj.flag].length == 0){
+                delete room[obj.flag];
+            }
         }
-        removeByValue(room[obj.flag],obj.num);
-    })
+        console.log('有玩家离开，当前在线玩家:' + onlineUsers + "房间数量:" + getJsonSize(room));
+    });
 
 
 });
 
 function removeByValue(array, val) {
-    for(var i=0; i<array.length; i++) {
-        if(array[i] == val) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == val) {
             array.splice(i, 1);
             break;
         }
     }
+}
+
+function getJsonSize(obj) {
+    var i = 0;
+    for(var o in obj){
+        i++;
+    }
+    return i;
 }
 
 
